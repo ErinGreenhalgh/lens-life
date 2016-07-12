b5028f7f684684c8d76e67f3e20b2a1392c87270
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-07-12 17:11:48 -0600

    tweak accessing info on auth_info -access info through hash correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ActiveRecord::Base
 
   def self.from_omniauth(auth_info)
-
     user = User.find_or_create_by(uid: auth_info[:uid])
     user.update_attributes(
       uid:         auth_info.uid,
-      nickname:    auth_info.nickname,
-      profile_pic: auth_info.profile_pic,
+      nickname:    auth_info.info.nickname,
+      profile_pic: auth_info.info.image,
       oauth_token: auth_info.credentials.token
     )
     user
